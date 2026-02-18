8b3f6a9aac6ab05ef3de4cb8fa652ade41258198
KaranSJ22 <karanjamkhandi@gmail.com>
2026-02-18 17:54:32 +0530

    feat:Add-Meet-Type-Route

--- backend/src/routes/admin.routes.js
+++ backend/src/routes/admin.routes.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const adminController = require('../controllers/adminController');
-
+const meetingTypeController=require('../controllers/meetingTypeController');
 router.get('/pending', adminController.getPendingBookings);
-
 router.post('/decide', adminController.decideBooking); 
 router.post('/hall', adminController.addHall);         
 router.post('/user', adminController.addUser);      
+router.post('/add-meet',meetingTypeController.addMeetingType);
 
 module.exports = router;
